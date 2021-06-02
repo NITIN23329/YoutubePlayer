27030b651f908472faa01636588842960a694f1c
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2021-06-02 22:16:07 +0530

    inflated activity_youtube and added a button programatically

--- app/src/main/java/com/nitin/youtubeplayer/YoutubeActivity.java
+++ app/src/main/java/com/nitin/youtubeplayer/YoutubeActivity.java
@@ -2,14 +2,39 @@ package com.nitin.youtubeplayer;
 
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
 
-public class YoutubeActivity extends YouTubeBaseActivity {
+public class YoutubeActivity extends YouTubeBaseActivity
+                implements YouTubePlayer.OnInitializedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_youtube);
+        // we get a instance of our activity_youtube and now we can update it in code. activity_youtube is indeed the Root  so pass null as its root
+        ConstraintLayout constraintLayout = (ConstraintLayout)getLayoutInflater().inflate(R.layout.activity_youtube,null,false);
+        setContentView(constraintLayout);
+
+        // adding a button to our layout in codes
+        Button button1 = new Button(this);  // its constructor needs a context and as our class YoutubeActivity extends activity(indirectly) , so we provided 'this'(object)
+        button1.setText("This button is created in codes");
+        button1.setLayoutParams(new ConstraintLayout.LayoutParams(300,80));
+        constraintLayout.addView(button1);
+
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
     }
 }
