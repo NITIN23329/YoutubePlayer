551508283dfa5efc35a925091db95781ab7e963c
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2021-06-04 00:13:06 +0530

    added PlayBackEvenetListener and PlayerStateChangeListener

--- app/src/main/java/com/nitin/youtubeplayer/YoutubeActivity.java
+++ app/src/main/java/com/nitin/youtubeplayer/YoutubeActivity.java
@@ -50,6 +50,9 @@ public class YoutubeActivity extends YouTubeBaseActivity
         // wasRestored is true if it has restored some previous session(video) and now we can resume the video
         Log.d(TAG, "onInitializationSuccess: provider is :" + provider.toString());
         Toast.makeText(this, "Youtube Player initialization Successful", Toast.LENGTH_SHORT).show();
+
+        youTubePlayer.setPlaybackEventListener(playbackEventListener);
+        youTubePlayer.setPlayerStateChangeListener(playerStateChangeListener);
         if (!wasRestored) {
             // we play a new video is another video was not resuming
             youTubePlayer.cueVideo(YOUTUBE_VIDEO_ID);
@@ -72,4 +75,57 @@ public class YoutubeActivity extends YouTubeBaseActivity
             Toast.makeText(this, errorMsg, Toast.LENGTH_LONG).show();
         }
     }
+
+    // PlaybackEventListener is an interface.
+    //Interface definition for callbacks that are invoked when video playback events occur.
+    private YouTubePlayer.PlaybackEventListener playbackEventListener = new YouTubePlayer.PlaybackEventListener() {
+        @Override
+        public void onPlaying() {
+            Toast.makeText(YoutubeActivity.this,"Video is playing :)",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPaused() {
+            Toast.makeText(YoutubeActivity.this,"Video is paused :(",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onStopped() {
+            Toast.makeText(YoutubeActivity.this,"Video got Stopped :?",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onBuffering(boolean b) {}
+
+        @Override
+        public void onSeekTo(int i) {}
+    };
+    //Interface definition for callbacks that are invoked when the high-level player state changes.
+    private YouTubePlayer.PlayerStateChangeListener playerStateChangeListener = new YouTubePlayer.PlayerStateChangeListener() {
+        @Override
+        public void onLoading() {
+            Toast.makeText(YoutubeActivity.this,"Wait, Video is Loading!",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onLoaded(String s) {}
+
+        @Override
+        public void onAdStarted() {
+            Toast.makeText(YoutubeActivity.this,"Ad is coming -_-",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onVideoStarted() {
+            Toast.makeText(YoutubeActivity.this,"Hi, Starting Video ;)",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onVideoEnded() {
+            Toast.makeText(YoutubeActivity.this,"Video ended, Bye ^_^",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onError(YouTubePlayer.ErrorReason errorReason) {}
+    };
 }
