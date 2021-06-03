a8120e67213f2aa9af79e3b331f543836c847ef5
NITIN KUMAR DAS <NITIN23329@GMAIL.COM>
2021-06-03 23:02:29 +0530

    handled youtube player initialization failure and successful

--- app/src/main/java/com/nitin/youtubeplayer/YoutubeActivity.java
+++ app/src/main/java/com/nitin/youtubeplayer/YoutubeActivity.java
@@ -1,10 +1,10 @@
 package com.nitin.youtubeplayer;
 
 
-import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
@@ -14,7 +14,9 @@ import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
 public class YoutubeActivity extends YouTubeBaseActivity
-                implements YouTubePlayer.OnInitializedListener {
+        implements YouTubePlayer.OnInitializedListener {
+
+    private static final String TAG = "YoutubeActivity";
 
     private final String GOOGLE_API_KEY = "AIzaSyCwbNdunJe1wz06Tjxxoels9nxSESjoCnY";
     private final String YOUTUBE_VIDEO_ID = "xLlvOo611xQ";
@@ -25,7 +27,7 @@ public class YoutubeActivity extends YouTubeBaseActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // we get a instance of our activity_youtube and now we can update it in code. activity_youtube is indeed the Root  so pass null as its root
-        ConstraintLayout constraintLayout = (ConstraintLayout)getLayoutInflater().inflate(R.layout.activity_youtube,null,false);
+        ConstraintLayout constraintLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null, false);
         setContentView(constraintLayout);
 
 //        // adding a button to our layout in codes
@@ -34,21 +36,40 @@ public class YoutubeActivity extends YouTubeBaseActivity
 //        button1.setLayoutParams(new ConstraintLayout.LayoutParams(700,120));    // this will set the width and height of the button1
 //        constraintLayout.addView(button1);
 
-        YouTubePlayerView player = new YouTubePlayerView(this);
-        player.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
-        constraintLayout.addView(player);
-
+        YouTubePlayerView playerView = new YouTubePlayerView(this);
+        playerView.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        constraintLayout.addView(playerView);
+        playerView.initialize(GOOGLE_API_KEY, this); // this will initialize our view to play youtube videos.
 
 
     }
 
-    @Override
-    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-
+    @Override  // called when initialization YoutubePlayer View succeeded
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
+        // youTubePlayer can be used to control video playbacks
+        // wasRestored is true if it has restored some previous session(video) and now we can resume the video
+        Log.d(TAG, "onInitializationSuccess: provider is :" + provider.toString());
+        Toast.makeText(this, "Youtube Player initialization Successful", Toast.LENGTH_SHORT).show();
+        if (!wasRestored) {
+            // we play a new video is another video was not resuming
+            youTubePlayer.cueVideo(YOUTUBE_VIDEO_ID);
+        }
     }
 
-    @Override
+    @Override   // called when initialization YoutubePlayer View failed
     public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        //YouTubeInitializationResult will provide the result of initialization, it will give the reason of failure
+        int REQUEST_CODE = 1; // it means we want the errorDialogue.
 
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            // this means the error we got during initialization of YoutubePlayerView can be fixed by user
+            // then a dialog is shown to user and on clicking them it will lead the user to somewhere he can fix the problem
+            youTubeInitializationResult.getErrorDialog(this, REQUEST_CODE).show();
+        } else {
+            // the error can't be fixed by user hence display a Toast(fading message at bottom).
+            // '(%1$s)' is used to get name of then error message
+            String errorMsg = String.format("Cann't initialize Youtube Player due to : (%1$s)", youTubeInitializationResult.toString());
+            Toast.makeText(this, errorMsg, Toast.LENGTH_LONG).show();
+        }
     }
 }
